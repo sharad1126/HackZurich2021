13a91ff5146c01b8be3c286b3e14fa1024bb3893
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2021-09-25 23:03:38 +0100

    API: cleaning code

--- API_for_Maps_Devs/GET.go
+++ API_for_Maps_Devs/GET.go
@@ -41,58 +41,14 @@ func (h *HttpServer) giveMe(ctx context.Context, city string, site string) http.
 		var data []pourDelivery
 
 		/*
-			data := pourDelivery{
-				Latitude:  53.2734,
-				Longitude: -7.77832031,
-				StartTime: "2021-09-25 19:53:00+00",
-				EndTime:   "2021-09-25 20:00:00+00",
-			}
-		*/
-		/*
 		* Calling relevant data from db
 		* Converting into JSON
 		* Sending
 		 */
 
-		if city == "boston" {
-
-			if site == "1" {
-				//data = "boston1"
-				fmt.Println("retriving boston 1")
-				data1, err := h.db.retriveSite(ctx, city, site)
-				data = data1
-				if err != nil {
-				}
-
-			} else if site == "2" {
-				//data = "boston2"
-				data1, err := h.db.retriveSite(ctx, city, site)
-				data = data1
-				if err != nil {
-				}
-
-			} else {
-				//data = "bostonAll"
-			}
-
-		} else if city == "toronto" {
-
-			if site == "1" {
-				//data = "toronto1"
-			} else if site == "2" {
-
-			} else {
-
-			}
-		} else if city == "dc" {
-
-			if site == "1" {
-
-			} else if site == "2" {
-
-			} else {
-
-			}
+		data1, err := h.db.retriveSite(ctx, city, site)
+		data = data1
+		if err != nil {
 		}
 
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
